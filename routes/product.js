492f144a36675607f8d3997fa9e2fc7486cbecb0
sanket bhojani <sanketbhojani107@gmail.com>
2026-05-12 10:18:10 +0530

    Add Swagger annotations for product routes

--- routes/product.js
+++ routes/product.js
@@ -5,9 +5,40 @@ import upload from '../middlewares/uploadMiddleware.js';
 import validate from '../middlewares/validateMiddleware.js';
 import { createProductSchema, updateProductSchema } from '../validators/productValidator.js';
 
-const router =express.Router();
+const router = express.Router();
 
+/**
+ * @swagger
+ * /product/getAllProducts:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     description: Retrieve a list of all products
+ *     responses:
+ *       200:
+ *         description: A list of products
+ */
 router.get('/getAllProducts',getAllProducts)
+
+/**
+ * @swagger
+ * /product/getProductById/{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The product ID
+ *     responses:
+ *       200:
+ *         description: Product data
+ *       404:
+ *         description: Product not found
+ */
 router.get('/getProductById/:id',getProductById)
 
 router.use(protect);
